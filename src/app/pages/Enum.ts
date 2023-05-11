e109a209132dd7c498e513eb89ba4e20cff33fa7
maiphuocloc <phuocloc_m@yahoo.com>
2023-05-11 16:32:49 +0700

    UI project detail

---
+++ src/app/pages/Enum.ts
@@ -0,0 +1,7 @@
+export {
+    state
+}
+const state = [
+    { id: 1, state: 'add-new', title: 'Thêm mới '},
+    { id: 2, state: 'detail', title: 'Chi tiết '},
+];
--- src/app/pages/project-manager/project-manager-detail/project-manager-detail.component.ts
+++ src/app/pages/project-manager/project-manager-detail/project-manager-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { state } from '../../Enum';
 
 @Component({
   selector: 'app-project-manager-detail',
@@ -7,9 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectManagerDetailComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute
+  ) { }
+
+  projectInfoForm = this.formBuilder.group({
+    projectName: ['', Validators.required],
+    projectCode: ['', Validators.required],
+  });
+
+  state = state;
+  title!: string;
+  id!: string;
 
   ngOnInit(): void {
+    var id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.id = id;
+      if (id == 'add-new') {
+        this.getTitle('add-new');
+        this.newFormAdd();
+      } else {
+        this.getTitle('detail');
+        this.getProductInfo(this.id);
+      }
+    }
+  }
+
+  getTitle(state: string) {
+    let title = this.state.find( x => x.state == state);
+    if (title) {
+      this.title = title?.title;
+    }
+  }
+
+  getProductInfo(id: string) {
+
+  }
+
+  newFormAdd() {
+
   }
 
 }
