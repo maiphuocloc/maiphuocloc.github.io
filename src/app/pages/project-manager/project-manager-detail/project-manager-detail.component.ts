ad75b08f2398151bdd7c3da6e9083acfd08e4005
maiphuocloc <phuocloc_m@yahoo.com>
2023-05-12 17:50:45 +0700

    create product document, update product list

--- src/app/pages/project-manager/project-manager-detail/project-manager-detail.component.ts
+++ src/app/pages/project-manager/project-manager-detail/project-manager-detail.component.ts
@@ -22,6 +22,79 @@ export class ProjectManagerDetailComponent implements OnInit {
     projectCode: ['', Validators.required],
   });
 
+  list_documents = [
+    {
+      id: 1,
+      list: [
+        { id: '1', title: 'PO', link: '', check: 'checked'},
+        { id: '2', title: 'Estimation', link: '', check: ''}
+      ],
+      document_type: 'project_document',
+      title: 'Tài liệu dự án'
+    },
+    {
+      id: 2,
+      list: [
+        { id: '1', title: 'Quyết định khởi động dự án', link: '', check: '' },
+        { id: '2', title: 'Quyết định thành lập đội dự án', link: '', check: ''}
+      ],
+      document_type: 'decision_document', 
+      title: 'Quyêt định'
+    },
+    {
+      id: 3,
+      list: [
+        { id: '1', title: 'Tài liệu SRD', link: '', check: ''},
+        { id: '2', title: 'Design', link: '', check: ''},
+        { id: '3', title: 'Kế hoạch', link: '', check: ''}
+      ],
+      document_type: 'technical_document',
+      title: 'Tài liệu kỹ thuật'
+    },
+    {
+      id: 4,
+      list: [
+        { id: '1', title: 'PO', link: '', check: ''},
+        { id: '2', title: 'Estimation', link: '', check: ''}
+      ],
+      document_type: 'meeting_document',
+      title: 'Nội dung các cuộc họp'
+    },
+    {
+      id: 5,
+      list: [
+        { id: '1', title: 'Báo cáo tuần 1', link: '', check: ''}
+      ],
+      document_type: 'report_document',
+      title: 'Báo cáo tiến độ hằng tuần'
+    },
+    {
+      id: 6,
+      list: [
+        { id: '1', title: 'Link Dev', link: '', check: ''},
+        { id: '2', title: 'Link Test', link: '', check: ''},
+        { id: '3', title: 'Link Prod', link: '', check: ''},
+        { id: '4', title: 'Database', link: '', check: ''},
+        { id: '5', title: 'Link GIT FE', link: '', check: ''},
+        { id: '6', title: 'Link GIT BE', link: '', check: ''}
+      ],
+      document_type: 'build_document',
+      title: 'Xây dựng dự án'
+    },
+    {
+      id: 7,
+      list: [
+        { id: '1', title: 'Biên bản nghiệm thu UAT', link: '', check: ''},
+        { id: '2', title: 'Biên bản nghiệm thu UAT tổng', link: '', check: ''},
+        { id: '3', title: 'Biên bản Golive', link: '', check: ''},
+        { id: '4', title: 'Biên bản bàn giao', link: '', check: ''},
+        { id: '5', title: 'Biên bản nghiệm thu tổng thể', link: '', check: ''}
+      ],
+      document_type: 'acceptance_document',
+      title: 'Biên bản nghiệm thu'
+    },
+  ]
+
   state = state;
   title!: string;
   id!: string;
@@ -42,7 +115,7 @@ export class ProjectManagerDetailComponent implements OnInit {
   }
 
   getTitle(state: string) {
-    let title = this.state.find( x => x.state == state);
+    let title = this.state.find(x => x.state == state);
     if (title) {
       this.title = title?.title;
     }
@@ -54,6 +127,11 @@ export class ProjectManagerDetailComponent implements OnInit {
 
   newFormAdd() {
 
+  }
+
+  addNewDocument(docType: string) {
+    console.log(docType);
+    
   }
 
   onFileLogoChanged(event: any): void {
